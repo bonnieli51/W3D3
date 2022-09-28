16d8d0407b46694aed78e8b6f833f870db772496
chazzlee <chazz@dev.com>
2022-09-28 15:12:00 -0400

    laptop is gonna die

--- recursion_exercises.rb
+++ recursion_exercises.rb
@@ -75,3 +75,29 @@ def rec_fibonnaci(n)
 end
 
 # p rec_fibonnaci(10)
+
+# bsearch([1, 3, 4, 5, 9], 5) # => 3
+def bsearch(arr, target)
+  middle_value = arr[arr.length / 2]
+  middle_index = arr.index(middle_value)
+  # left = arr[0...middle_index]
+  # right = arr[middle_index + 1..]
+  # p right
+
+  return nil if arr.length == 1 && middle_value != target
+  if target == middle_value
+    return arr.index(target)
+  elsif target < middle_value
+    bsearch(arr[0...middle_index], target)
+  elsif target > middle_value
+    bsearch(arr[middle_index+1..-1], target)
+  end
+end
+
+# p bsearch([1, 2, 3], 1) # => 0
+# p bsearch([2, 3, 4, 5], 3) # => 1
+# p bsearch([2, 4, 6, 8, 10], 6) # => 2
+p bsearch([1, 3, 4, 5, 9], 5) # => 3
+# p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+# p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+# p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
