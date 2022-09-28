b1ee8c326d68908d952fe10ba0c283d41c511723
Bonnie Li <bonnieli@Bonnies-MacBook-Pro.local>
2022-09-28 17:22:24 -0400

    merge_sort comp

--- recursion_exercises.rb
+++ recursion_exercises.rb
@@ -95,10 +95,32 @@ def bsearch(arr, target)
   end
 end
 
-p bsearch([1, 2, 3], 1) # => 0
-p bsearch([2, 3, 4, 5], 3) # => 1
-p bsearch([2, 4, 6, 8, 10], 6) # => 2
-p bsearch([1, 3, 4, 5, 9], 5) # => 3
-p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
-p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
-p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
+
+def merge_sort(arr)
+    return arr if arr.length == 1
+    middle_index = arr.length / 2 - 1
+    left = arr[0..middle_index]
+    right = arr[middle_index+1..]
+    merge(merge_sort(left), merge_sort(right))   
+end
+
+def merge(left, right)
+    return left if right.empty?
+    return right if left.empty?
+
+   small = if left.first <= right.first
+        left.shift
+   else
+        right.shift
+   end
+    [small] + merge(left,right)
+end
+
+p merge_sort([38,27,43,3,9,82,10])
+# p bsearch([1, 2, 3], 1) # => 0
+# p bsearch([2, 3, 4, 5], 3) # => 1
+# p bsearch([2, 4, 6, 8, 10], 6) # => 2
+# p bsearch([1, 3, 4, 5, 9], 5) # => 3
+# p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+# p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+# p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
