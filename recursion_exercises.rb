58c396386d6435bc00aebb9991e7f1d5418aa874
chazzlee <chazz@dev.com>
2022-09-28 14:28:00 -0400

    gitignore

--- recursion_exercises.rb
+++ recursion_exercises.rb
@@ -1,51 +1,52 @@
-require "byebug"
+require 'byebug'
+
 def range(start, last)
-    #return array of nums in range
-   return [] if start == last # 5,5
-   [start] +  range(start +1, last)
+  # return array of nums in range
+  return [] if start == last # 5,5
+
+  [start] + range(start + 1, last)
 end
 
 # p range(1,5)
 # p range(1,10)
 
-def exponentiation_1(base, exp) 
-    return 1 if exp == 0
-    base * exponentiation_1(base,exp-1)
+def exponentiation_1(base, exp)
+  return 1 if exp == 0
 
+  base * exponentiation_1(base, exp - 1)
 end
 
 # p exponentiation_1(3,12)
 
 def exponentiation_2(base, n)
-    return 1 if n == 0
-    return base if n == 1
-    if n.even?
-        exponentiation_2(base, n/2)**2
-    else
-        base * (exponentiation_2(base, (n - 1)/2)**2)
-    end
+  return 1 if n == 0
+  return base if n == 1
+
+  if n.even?
+    exponentiation_2(base, n / 2)**2
+  else
+    base * (exponentiation_2(base, (n - 1) / 2)**2)
+  end
 end
 
 # p exponentiation_2(2,5)
 class Array
-    def deep_dup
-        result = []
-        self.each do |ele| # ele is array
-            
-            if ele.is_a?(Array)
-                result << ele.deep_dup
-            else
-                result << ele
-            end
-
-        end
-        result
+  def deep_dup
+    result = []
+    each do |ele| # ele is array
+      result << if ele.is_a?(Array)
+                  ele.deep_dup
+                else
+                  ele
+                end
     end
+    result
+  end
 end
 
 robot_parts = [
-  ["nuts", "bolts", "washers"],
-  ["capacitors", "resistors", "inductors"]
+  %w[nuts bolts washers],
+  %w[capacitors resistors inductors]
 ]
 
 # robot_parts_copy = robot_parts.deep_dup
